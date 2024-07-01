1013d635ea43515ce05829078db4180472206cc7
lolgh57 <sasha.over.game@gmail.com>
2024-07-01 20:18:02 +0200

    create ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: "telegram-bot",
+      script: "nodemon",
+      args: "index.js",
+      watch: true,
+      ignore_watch: ["node_modules"],
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
